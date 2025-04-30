b72da575e73e8e09cca8b172d805727e9a2cafcf
DimaM190 <dima.mahuga@gmail.com>
2025-04-30 16:25:24 +0300

    HW_2.1

---
+++ home_w_2_1.py
@@ -0,0 +1,20 @@
+from pywebio.input import NUMBER
+
+from pywebio.input import input as pw_input
+
+from pywebio.output import put_html, put_success
+
+# HEADER
+put_html("<hi>Enter a 4-digit number</h1>")
+
+user_input = pw_input(type="text", required=True, minlength=4, maxlength=4)
+if user_input.isdigit():
+    user_input = list(user_input)
+    put_success(
+        f"Your inverted number:\n{user_input[3]}\n{user_input[2]}\n{user_input[1]}\n{user_input[0]}"
+    )
+else:
+    put_success("Error: You have extraneous characters")
+
+
+pass
